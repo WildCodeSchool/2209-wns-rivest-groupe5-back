778e25d2adeb3b1ec211f9dce41338056826629f
witzkvn <witz.kevin05@gmail.com>
2022-11-15 14:35:35 +0100

    ajout de l'entité user, avec resolver de base, et mise en place du server de base avec graphql

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
+import "reflect-metadata";
+import { ApolloServer } from "apollo-server";
+import dataSource from "./utils/datasource";
+import { buildSchema } from "type-graphql";
+import { UserResolver } from "./resolvers/userResolver";
+
 const port = 5050;
+
+async function start(): Promise<void> {
+  try {
+    await dataSource.initialize();
+
+    const schema = await buildSchema({
+      resolvers: [UserResolver],
+    });
+    const server = new ApolloServer({ schema });
+
+    try {
+      const { url }: { url: string } = await server.listen({ port });
+      console.log(`🚀  Server ready at ${url}`);
+    } catch (error) {
+      console.log(error);
+    }
+  } catch (error) {
+    console.log("Error while launching the server");
+    console.log(error);
+  }
+}
+
+start();
