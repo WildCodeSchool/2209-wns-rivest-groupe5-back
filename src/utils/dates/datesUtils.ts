d8bad5a9ac66e54763f282b7891a4b2509836f69
Witz Kevin <90613127+witzkvn@users.noreply.github.com>
2023-02-02 09:50:03 +0100

    WCS-91C: bar chart month (#20)
    * ajout resolver pour stats activities 4 dernieres semaines
    
    * adjustments for month chart data

--- src/utils/dates/datesUtils.ts
+++ src/utils/dates/datesUtils.ts
@@ -9,16 +9,16 @@ export const getDateXDaysAgo = (daysAgo: number): Date => {
   return targetDate;
 };
 
-interface dayInfos {
+interface periodInfos {
   start: Date;
   end: Date;
   name: string;
 }
 
-export const getDateXDaysAgoDaysInfos = (daysAgo: number): dayInfos[] => {
+export const getDateXDaysAgoDaysInfos = (daysAgo: number): periodInfos[] => {
   const now = new Date();
 
-  const lastXDaysInfos: dayInfos[] = [];
+  const lastXDaysInfos: periodInfos[] = [];
   // daysAgo - 1 to include currentDay
   const oldestDate = new Date(
     now.getTime() - (daysAgo - 1) * 24 * 60 * 60 * 1000
@@ -39,4 +39,42 @@ export const getDateXDaysAgoDaysInfos = (daysAgo: number): dayInfos[] => {
   }
 
   return lastXDaysInfos;
+};
+
+export const getDateXWeeksAgoInfos = (weeksAgo: number): periodInfos[] => {
+  const now = new Date();
+
+  const lastXWeeksInfos: periodInfos[] = [];
+  const oldestDate = new Date(
+    now.getTime() - weeksAgo * 7 * 24 * 60 * 60 * 1000
+  );
+
+  for (let i = 0; i < weeksAgo * 7; i += 7) {
+    const baseDateMS = oldestDate.getTime();
+    const newDayDate = baseDateMS + i * 3600 * 24 * 1000;
+    const date = new Date(newDayDate);
+
+    const name =
+      weeksAgo * 7 - i === 7
+        ? "Cette semaine"
+        : weeksAgo * 7 - i === 14
+        ? "Il y a une semaine"
+        : `Il y a ${(weeksAgo * 7 - i) / 7 - 1} semaines`;
+
+    const startOfTheWeekDate = new Date(date.setUTCHours(0, 0, 0, 0));
+    const endOfFirstWeekDayDate = new Date(date.setUTCHours(23, 59, 59, 999));
+    const endOfTheWeekDate = new Date(
+      endOfFirstWeekDayDate.setDate(endOfFirstWeekDayDate.getDate() + 7)
+    );
+
+    const dayInfos = {
+      start: startOfTheWeekDate,
+      end: endOfTheWeekDate,
+      name,
+    };
+
+    lastXWeeksInfos.push(dayInfos);
+  }
+
+  return lastXWeeksInfos;
 };
