f52ac08c6fc3810d204b14fb66dacfea0ee7250c
witzkvn <witz.kevin05@gmail.com>
2023-03-02 16:32:51 +0100

    ajout champs visibility sur user et entite following pour les liens entre users

--- back/src/entities/user.ts
+++ back/src/entities/user.ts
@@ -1,10 +1,10 @@
 import { Field, ObjectType } from "type-graphql";
 import {
-  Column,
-  CreateDateColumn,
-  Entity,
-  OneToMany,
-  PrimaryGeneratedColumn,
+    Column,
+    CreateDateColumn,
+    Entity,
+    OneToMany,
+    PrimaryGeneratedColumn,
 } from "typeorm";
 import { IUser } from "../interfaces/entities/IUser";
 import { Activity } from "./activity";
@@ -13,76 +13,92 @@ import { GoodDeal } from "./goodDeal";
 import crypto from "crypto";
 import { USER_ROLES } from "../utils/userRoles";
 import { GoodDealVote } from "./gooDealVote";
+import { userVisibility } from "../interfaces/entities/UserVisibilityOptions";
+import { Following } from "./userIsFollowing";
 
 @ObjectType()
 @Entity()
 export class User implements IUser {
-  @Field()
-  @PrimaryGeneratedColumn()
-  userId: number;
+    @Field()
+    @PrimaryGeneratedColumn()
+    userId: number;
 
-  @Field()
-  @Column()
-  firstname: string;
+    @Field()
+    @Column()
+    firstname: string;
 
-  @Field()
-  @Column()
-  lastname: string;
+    @Field()
+    @Column()
+    lastname: string;
 
-  @Field()
-  @Column({ unique: true })
-  email: string;
+    @Field()
+    @Column({ unique: true })
+    email: string;
 
-  @Field()
-  @Column()
-  password: string;
+    @Field()
+    @Column()
+    password: string;
 
-  @Field({ nullable: true })
-  @Column({ nullable: true })
-  avatar?: string;
+    @Field({ nullable: true })
+    @Column({ nullable: true })
+    avatar?: string;
 
-  @Field()
-  @CreateDateColumn({ name: "createdAt" })
-  createdAt: Date;
+    @Field()
+    @CreateDateColumn({ name: "createdAt" })
+    createdAt: Date;
 
-  @Field(() => [GoodDeal], { nullable: true })
-  @OneToMany(() => GoodDeal, (goodDeal) => goodDeal.user, {
-    cascade: true,
-  })
-  goodDeals: GoodDeal[];
+    @Field()
+    @Column({
+        type: "enum",
+        enum: userVisibility,
+        default: userVisibility.private,
+    })
+    name: userVisibility;
 
-  @Field(() => [GoodDealVote], { nullable: true })
-  @OneToMany(() => GoodDealVote, (goodDealVote) => goodDealVote.user, {
-    cascade: true,
-  })
-  goodDealVotes: GoodDealVote[];
+    @Field(() => [GoodDeal], { nullable: true })
+    @OneToMany(() => GoodDeal, (goodDeal) => goodDeal.user, {
+        cascade: true,
+    })
+    goodDeals: GoodDeal[];
 
-  @Field(() => [Activity], { nullable: true })
-  @OneToMany(() => Activity, (activity) => activity.user, {
-    cascade: true,
-  })
-  activities: Activity[];
+    @Field(() => [GoodDealVote], { nullable: true })
+    @OneToMany(() => GoodDealVote, (goodDealVote) => goodDealVote.user, {
+        cascade: true,
+    })
+    goodDealVotes: GoodDealVote[];
 
-  @Field(() => [Contribution], { nullable: true })
-  @OneToMany(() => Contribution, (contribution) => contribution.user, {
-    cascade: true,
-  })
-  contributions: Contribution[];
+    @Field(() => [Activity], { nullable: true })
+    @OneToMany(() => Activity, (activity) => activity.user, {
+        cascade: true,
+    })
+    activities: Activity[];
 
-  @Field({ nullable: true })
-  @Column({ nullable: true })
-  passwordResetToken: string;
+    @Field(() => [Contribution], { nullable: true })
+    @OneToMany(() => Contribution, (contribution) => contribution.user, {
+        cascade: true,
+    })
+    contributions: Contribution[];
 
-  @Field({ nullable: true })
-  @Column({ nullable: true })
-  passwordResetExpires: Date;
+    @OneToMany(() => Following, (following) => following.user)
+    followings: Following[];
 
-  @Column()
-  role: USER_ROLES;
+    @OneToMany(() => Following, (following) => following.userFollowed)
+    followers: Following[];
 
-  public get createPasswordResetToken(): string {
-    const resetToken = crypto.randomBytes(32).toString("hex");
+    @Field({ nullable: true })
+    @Column({ nullable: true })
+    passwordResetToken: string;
 
-    return resetToken;
-  }
+    @Field({ nullable: true })
+    @Column({ nullable: true })
+    passwordResetExpires: Date;
+
+    @Column()
+    role: USER_ROLES;
+
+    public get createPasswordResetToken(): string {
+        const resetToken = crypto.randomBytes(32).toString("hex");
+
+        return resetToken;
+    }
 }
---
+++ back/src/interfaces/entities/IFollowing.ts
@@ -0,0 +1,6 @@
+import { User } from "../../entities/user";
+
+export interface IFollowing {
+    user: number;
+    userFollowed: number;
+}
