e3c635cfecd13bdad17da81b812344411fe7244f
Quentin Delon <quentin.delon36@gmail.com>
2023-03-29 14:21:53 +0200

    change postgres pwd

--- back/src/utils/datasource.ts
+++ back/src/utils/datasource.ts
@@ -12,7 +12,7 @@ const dataSource = new DataSource({
     host: process.env.DB || "db",
     port: 5432,
     username: "postgres",
-    password: "example",
+    password: "azerty",
     database: "postgres",
     synchronize: true,
     entities: [
