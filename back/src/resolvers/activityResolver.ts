1d6f8273e7929b6809de88ea06b783dc1aa272db
Vanessa <salvadoribanez.vanessa@gmail.com>
2023-04-27 12:28:16 +0200

    WCS-122: created delete resolver to delete activities

--- back/src/resolvers/activityResolver.ts
+++ back/src/resolvers/activityResolver.ts
@@ -15,178 +15,216 @@ import { IObj } from "../interfaces/general/IObject";
 
 @Resolver(Activity)
 export class ActivityResolver {
-    @Authorized()
-    @Query(() => [Activity])
-    async getAllMyActivities(@Ctx() ctx: Context): Promise<Activity[]> {
-        const userFromCtx = ctx as IUserCtx;
+  @Authorized()
+  @Query(() => [Activity])
+  async getAllMyActivities(@Ctx() ctx: Context): Promise<Activity[]> {
+    const userFromCtx = ctx as IUserCtx;
 
-        const allActivities = await dataSource.getRepository(Activity).find({
-            relations: {
-                activityType: true,
-                user: true,
+    const allActivities = await dataSource.getRepository(Activity).find({
+      relations: {
+        activityType: true,
+        user: true,
+      },
+      where: {
+        user: {
+          userId: userFromCtx.user.userId,
+        },
+      },
+    });
+
+    return allActivities;
+  }
+
+  @Authorized()
+  @Mutation(() => Activity)
+  async createActivity(
+    @Ctx() ctx: Context,
+    @Arg("data") createActivity: CreateActivityInput
+  ): Promise<Activity> {
+    const userFromCtx = ctx as IUserCtx;
+
+    if (createActivity.carbonQuantity <= 0) {
+      throw new ApolloError(
+        "La quantité de carbone émise doit être supérieure à 0."
+      );
+    }
+
+    const activityTypeFromDb = await dataSource
+      .getRepository(ActivityType)
+      .findOneByOrFail({
+        activityTypeId: createActivity.activityTypeId,
+      });
+
+    const newActivity = new Activity();
+    newActivity.title = createActivity.title;
+    newActivity.activityDate = createActivity.activityDate;
+    newActivity.carbonQuantity = createActivity.carbonQuantity;
+    newActivity.description = createActivity.description;
+    newActivity.activityType = activityTypeFromDb;
+    newActivity.user = userFromCtx.user as User;
+    newActivity.createdAt = new Date();
+
+    const activityFromDB = await dataSource.manager.save(Activity, newActivity);
+
+    return activityFromDB;
+  }
+
+  @Authorized()
+  @Mutation(() => Activity)
+  async updateActivity(
+    @Ctx() ctx: Context,
+    @Arg("activityId") activityId: number,
+    @Arg("data") updateActivity: UpdateActivityInput
+  ): Promise<Activity> {
+    const userFromCtx = ctx as IUserCtx;
+
+    const activityFromDb = await dataSource.manager.find(Activity, {
+      where: {
+        activityId: activityId,
+      },
+      relations: {
+        user: true,
+      },
+    });
+
+    if (activityFromDb === undefined || activityFromDb[0] === undefined) {
+      throw new Error("Not activity found with this activityId");
+    }
+
+    if (
+      userFromCtx.user.userId !== activityFromDb[0].user.userId &&
+      userFromCtx.user.role !== USER_ROLES.ADMIN
+    ) {
+      // if user requesting is not the author of the activity to update and is not admin, throw error
+      throw new Error(
+        "The user trying to update the activity is not the activity creator and is not an admin. This action is forbidden."
+      );
+    }
+
+    let newActivityTypeFromDb = activityFromDb[0].activityType;
+    if (updateActivity.activityTypeId !== undefined) {
+      newActivityTypeFromDb = await dataSource
+        .getRepository(ActivityType)
+        .findOneByOrFail({
+          activityTypeId: updateActivity.activityTypeId,
+        });
+    }
+
+    const updatedActivity = await dataSource.getRepository(Activity).update(
+      { activityId: activityId },
+      {
+        activityType: newActivityTypeFromDb,
+        title: updateActivity.title,
+        activityDate: updateActivity.activityDate,
+        carbonQuantity: updateActivity.carbonQuantity,
+        description: updateActivity.description,
+      }
+    );
+
+    if (updatedActivity.affected === 0) {
+      throw new Error("Could not update the Activity.");
+    }
+
+    // find again activity to get the updated version
+    const activity = await dataSource
+      .getRepository(Activity)
+      .findOneByOrFail({ activityId });
+
+    return activity;
+  }
+
+  @Authorized()
+  @Mutation(() => String)
+  async deleteActivity(
+    @Ctx() ctx: Context,
+    @Arg("activityId") activityId: number
+  ): Promise<string> {
+    const userFromCtx = ctx as IUserCtx;
+
+    const activityFromDb = await dataSource.manager.find(Activity, {
+      where: {
+        activityId: activityId,
+      },
+      relations: {
+        user: true,
+      },
+    });
+
+    if (activityFromDb === undefined || activityFromDb[0] === undefined) {
+      throw new Error("Not activity found with this activityId");
+    }
+
+    if (userFromCtx.user.userId !== activityFromDb[0].user.userId) {
+      // if user requesting is not the author of the activity to delete, throw error
+      throw new Error(
+        "The user trying to delete the activity is not the activity creator. This action is forbidden."
+      );
+    }
+
+    const deletedActivity = await dataSource
+      .getRepository(Activity)
+      .delete({ activityId: activityId });
+
+    if (deletedActivity.affected === 0) {
+      throw new Error("Could not delete the Activity.");
+    }
+
+    return "Activity deleted";
+  }
+
+  @Authorized()
+  @Query(() => [Activity])
+  async getAllUsersFollowedLastSevenDaysActivities(
+    @Ctx() ctx: Context
+  ): Promise<Activity[]> {
+    const userFromCtx = ctx as IUserCtx;
+
+    const followedUsers = await dataSource.getRepository(Following).find({
+      where: { user: userFromCtx.user.userId },
+      select: {
+        userFollowed: true,
+      },
+    });
+    const followedUserIds = followedUsers.map(
+      (follow: IObj) => follow.userFollowed
+    );
+
+    const targetDate = getDateXDaysAgo(7);
+    let allUsersActivities: Activity[] = [];
+
+    for await (const userId of followedUserIds) {
+      const userLastActivities: Activity[] = await dataSource
+        .getRepository(Activity)
+        .find({
+          relations: {
+            activityType: true,
+            user: true,
+          },
+          where: {
+            user: {
+              userId: userId,
             },
-            where: {
-                user: {
-                    userId: userFromCtx.user.userId,
-                },
+            createdAt: MoreThan(targetDate),
+          },
+          select: {
+            user: {
+              userId: true,
+              firstname: true,
+              lastname: true,
+              email: true,
+              avatar: true,
             },
+          },
         });
 
-        return allActivities;
+      allUsersActivities = allUsersActivities.concat(userLastActivities);
     }
 
-    @Authorized()
-    @Mutation(() => Activity)
-    async createActivity(
-        @Ctx() ctx: Context,
-        @Arg("data") createActivity: CreateActivityInput
-    ): Promise<Activity> {
-        const userFromCtx = ctx as IUserCtx;
+    // order all activities from most recent to oldest
+    allUsersActivities.sort(
+      (a, b) => b.createdAt.getTime() - a.createdAt.getTime()
+    );
 
-        if (createActivity.carbonQuantity <= 0) {
-            throw new ApolloError(
-                "La quantité de carbone émise doit être supérieure à 0."
-            );
-        }
-
-        const activityTypeFromDb = await dataSource
-            .getRepository(ActivityType)
-            .findOneByOrFail({
-                activityTypeId: createActivity.activityTypeId,
-            });
-
-        const newActivity = new Activity();
-        newActivity.title = createActivity.title;
-        newActivity.activityDate = createActivity.activityDate;
-        newActivity.carbonQuantity = createActivity.carbonQuantity;
-        newActivity.description = createActivity.description;
-        newActivity.activityType = activityTypeFromDb;
-        newActivity.user = userFromCtx.user as User;
-        newActivity.createdAt = new Date();
-
-        const activityFromDB = await dataSource.manager.save(
-            Activity,
-            newActivity
-        );
-
-        return activityFromDB;
-    }
-
-    @Authorized()
-    @Mutation(() => Activity)
-    async updateActivity(
-        @Ctx() ctx: Context,
-        @Arg("activityId") activityId: number,
-        @Arg("data") updateActivity: UpdateActivityInput
-    ): Promise<Activity> {
-        const userFromCtx = ctx as IUserCtx;
-
-        const activityFromDb = await dataSource.manager.find(Activity, {
-            where: {
-                activityId: activityId,
-            },
-            relations: {
-                user: true,
-            },
-        });
-
-        if (
-            activityFromDb !== undefined &&
-            activityFromDb[0] !== undefined &&
-            userFromCtx.user.userId !== activityFromDb[0].user.userId &&
-            userFromCtx.user.role !== USER_ROLES.ADMIN
-        ) {
-            // if user requesting is not the author of the activity to update and is not admin, throw error
-            throw new Error(
-                "The user trying to update the activity is not the activity creator and is not an admin. This action is forbidden."
-            );
-        }
-
-        let newActivityTypeFromDb = activityFromDb[0].activityType;
-        if (updateActivity.activityTypeId !== undefined) {
-            newActivityTypeFromDb = await dataSource
-                .getRepository(ActivityType)
-                .findOneByOrFail({
-                    activityTypeId: updateActivity.activityTypeId,
-                });
-        }
-
-        const updatedActivity = await dataSource.getRepository(Activity).update(
-            { activityId: activityId },
-            {
-                activityType: newActivityTypeFromDb,
-                title: updateActivity.title,
-                activityDate: updateActivity.activityDate,
-                carbonQuantity: updateActivity.carbonQuantity,
-                description: updateActivity.description,
-            }
-        );
-
-        if (updatedActivity.affected === 0) {
-            throw new Error("Could not update the Activity.");
-        }
-
-        // find again activity to get the updated version
-        const activity = await dataSource
-            .getRepository(Activity)
-            .findOneByOrFail({ activityId });
-
-        return activity;
-    }
-
-    @Authorized()
-    @Query(() => [Activity])
-    async getAllUsersFollowedLastSevenDaysActivities(
-        @Ctx() ctx: Context
-    ): Promise<Activity[]> {
-        const userFromCtx = ctx as IUserCtx;
-
-        const followedUsers = await dataSource.getRepository(Following).find({
-            where: { user: userFromCtx.user.userId },
-            select: {
-                userFollowed: true,
-            },
-        });
-        const followedUserIds = followedUsers.map(
-            (follow: IObj) => follow.userFollowed
-        );
-
-        const targetDate = getDateXDaysAgo(7);
-        let allUsersActivities: Activity[] = [];
-
-        for await (const userId of followedUserIds) {
-            const userLastActivities: Activity[] = await dataSource
-                .getRepository(Activity)
-                .find({
-                    relations: {
-                        activityType: true,
-                        user: true,
-                    },
-                    where: {
-                        user: {
-                            userId: userId,
-                        },
-                        createdAt: MoreThan(targetDate),
-                    },
-                    select: {
-                        user: {
-                            userId: true,
-                            firstname: true,
-                            lastname: true,
-                            email: true,
-                            avatar: true,
-                        },
-                    },
-                });
-
-            allUsersActivities = allUsersActivities.concat(userLastActivities);
-        }
-
-        // order all activities from most recent to oldest
-        allUsersActivities.sort(
-            (a, b) => b.createdAt.getTime() - a.createdAt.getTime()
-        );
-
-        return allUsersActivities;
-    }
+    return allUsersActivities;
+  }
 }
